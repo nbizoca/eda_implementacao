f7e7f82306731a7d062c3d81ebfd0cea5c92f906
lucas310fernando-eng <lucas310fernando@gmail.com>
2026-08-02 18:24:55 -0300

    Métodos isEmpty e isFull.

--- src/estruturas/fila/FilaImpl.java
+++ src/estruturas/fila/FilaImpl.java
@@ -15,16 +15,21 @@ public class FilaImpl<T> implements Fila<T> {
         if (capacidade <= 0) {
             throw new IllegalArgumentException("A capacidade da fila deve ser maior que zero.");
         }
-
         this.capacidade = capacidade;
-        this.elementos = (T[]) new Object[capacidade]; // Criação do array genérico
-
+        this.elementos = (T[]) new Object[capacidade]; // Criação do array genérico e o casting.
         // Configuração inicial da fila vazia
         this.inicio = 0;   // O primeiro a sair estará no índice 0
         this.fim = -1;     // O fim começa em -1 (pois ainda não inserimos nada)
         this.tamanho = 0;  // Obviamente, começamos com zero elementos
     }
 
-    // O IntelliJ vai apontar erros aqui pedindo os métodos da interface.
-    // Faremos isso no próximo passo!
+    @Override
+    public boolean isEmpty() {
+        return this.tamanho == 0;//Vai ser vazia se a fila tem tamanho 0.
+    }
+
+    @Override
+    public boolean isFull() {
+        return this.tamanho == this.capacidade;//Vai ser cheia se o tamanho da fila atinge a capacidade.
+    }
 }
