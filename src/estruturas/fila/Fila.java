4c582a970f0767fafc4fd43a7a0d1095b3566f51
lucas310fernando-eng <lucas310fernando@gmail.com>
2026-08-02 18:00:38 -0300

    Implementando a interface para fila

--- src/estruturas/fila/Fila.java
+++ src/estruturas/fila/Fila.java
@@ -1,4 +1,19 @@
 package estruturas.fila;
 
-public interface Fila {
+public interface Fila<T> {
+
+    // Adiciona um elemento no final da fila (equivalente ao push)
+    void enqueue(T elemento);
+
+    // Remove e retorna o elemento do início da fila (equivalente ao pop)
+    T dequeue();
+
+    // Apenas retorna (visualiza) o elemento do início da fila, sem remover (equivalente ao top)
+    T head();
+
+    // Verifica se a fila está vazia
+    boolean isEmpty();
+
+    // Verifica se a fila está cheia
+    boolean isFull();
 }
