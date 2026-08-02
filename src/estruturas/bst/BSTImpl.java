d9fb6089dcc8e60337a6f0a6cdb21369a7ecca77
Bia <biagomes96100@gmail.com>
2026-08-02 17:18:37 -0300

    adicionando classe para o nó da árvore

--- src/estruturas/bst/BSTImpl.java
+++ src/estruturas/bst/BSTImpl.java
@@ -1,4 +1,53 @@
 package estruturas.bst;
 
-public class BSTImpl {
+public class BTImpl extends BTNode implements BT{
+    @Override
+    public BTNode getRoot() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int height() {
+        return 0;
+    }
+
+    @Override
+    public BTNode search(Object elem) {
+        return null;
+    }
+
+    @Override
+    public void insert(Object value) {
+
+    }
+
+    @Override
+    public void remove(Object key) {
+
+    }
+
+    @Override
+    public Object[] preOrder() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] order() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] postOrder() {
+        return new Object[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
