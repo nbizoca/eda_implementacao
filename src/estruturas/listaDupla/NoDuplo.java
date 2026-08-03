e11b75dbe2e639be5e49b114aca06d8c593d4e95
lucas310fernando-eng <lucas310fernando@gmail.com>
2026-08-03 14:14:10 -0300

    Adcionado estrutura do nóduplo.

--- src/estruturas/listaDupla/NoDuplo.java
+++ src/estruturas/listaDupla/NoDuplo.java
@@ -1,4 +1,51 @@
 package estruturas.listaDupla;
 
-public class NoDuplo {
+public class NoDuplo<T> {
+
+    // A "carga" transportada pelo vagão
+    private T dado;
+
+    // O engate que aponta para o vagão da FRENTE
+    private NoDuplo<T> proximo;
+
+    // A GRANDE NOVIDADE: O engate que aponta para o vagão de TRÁS
+    private NoDuplo<T> anterior;
+
+    /**
+     * Construtor: Chamado toda vez que criamos um novo Nó Duplo.
+     * @param dado A informação que queremos guardar.
+     */
+    public NoDuplo(T dado) {
+        this.dado = dado;
+        // Quando um vagão é construído, seus engates ainda estão soltos
+        this.proximo = null;
+        this.anterior = null;
+    }
+
+    // --- Getters e Setters ---
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    public NoDuplo<T> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NoDuplo<T> proximo) {
+        this.proximo = proximo;
+    }
+
+    // Novos métodos para gerenciar o engate traseiro
+    public NoDuplo<T> getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NoDuplo<T> anterior) {
+        this.anterior = anterior;
+    }
 }
